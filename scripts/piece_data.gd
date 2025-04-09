858d1f74b4cdc4a547cefb4e1caf35ba428a01a7
MetalCRT <metalcraftx@gmail.com>
2025-04-09 09:20:08 -0400

    Feat: Tetris Game Logic

--- scripts/piece_data.gd
+++ scripts/piece_data.gd
@@ -1,15 +1,5 @@
-extends Node
+extends Resource
 
-enum Tetromino {
-	I, O, T, J, L, S, Z
-}
-
-var cells = {
-	Tetromino.I: [Vector2(-1, 0), Vector2(0, 0), Vector2(1, 0), Vector2(2, 0)],
-	Tetromino.J: [Vector2(-1, 1), Vector2(-1, 0), Vector2(0, 0), Vector2(1, 0)],
-	Tetromino.L: [Vector2(1, 1), Vector2(-1, 0), Vector2(0, 0), Vector2(1, 0)],
-	Tetromino.O: [Vector2(0, 1), Vector2(1, 1), Vector2(0, 0), Vector2(1, 0)],
-	Tetromino.S: [Vector2(0, 1), Vector2(1, 1), Vector2(-1, 0), Vector2(0, 0)],
-	Tetromino.T: [Vector2(0, 1), Vector2(-1, 0), Vector2(0, 0), Vector2(1, 0)],
-	Tetromino.Z: [Vector2(-1, 1), Vector2(0, 1), Vector2(0, 0), Vector2(1, 0)]
-}
+@export var piece_texture: Texture
+@export var spawn_position: Vector2
+@export var tetromino_type: Shared.Tetromino
---
+++ scripts/piece_spawner.gd
@@ -0,0 +1,34 @@
+extends Node
+
+var current_tetromino
+var bag = []
+var next_tetromino
+@onready var board = $"../Board" as Board
+
+#enum Tetromino { I, O, T, J, L, S, Z }
+
+func _ready():
+	#fill_bag()
+	#spawn_next_piece()
+	current_tetromino = Shared.Tetromino.values().pick_random()
+	next_tetromino = Shared.Tetromino.values().pick_random()
+	board.spawn_tetromino(current_tetromino, false, null)
+	board.spawn_tetromino(next_tetromino, true, Vector2(100, 50))
+	board.tetromino_locked.connect(on_tetromino_locked)
+
+
+func on_tetromino_locked():
+	
+	current_tetromino = next_tetromino
+	next_tetromino = Shared.Tetromino.values().pick_random()
+	board.spawn_tetromino(current_tetromino, false, null)
+	board.spawn_tetromino(next_tetromino, true, Vector2(100, 50))
+
+func fill_bag():
+	bag = Shared.Tetromino.values()
+	bag.shuffle()
+
+func spawn_next_piece():
+	if bag.size() == 0:
+		fill_bag()
+	current_tetromino = bag.pop_front()
